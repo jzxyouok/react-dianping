93a536bf307ed8c0da71bc9ea363100abcb173cc
dengzhao <1301829682@qq.com>
2017-06-27 23:50:13 +0800

    搭建webpack+React开发环境

---
+++ 1：搭建 webpack+React开发环境/app/App.js
@@ -0,0 +1,14 @@
+/**
+ * Created by litong on 2017/6/6.
+ */
+import React from 'react'
+
+class App extends React.Component{
+    render() {
+        return (
+            <div>搭建 webpack+React 开发环境参考文档http://www.imooc.com/article/14499</div>    
+        )
+    }
+}
+
+export default App
